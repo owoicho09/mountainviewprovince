5c9d86f77097e6c01f1d60761fda213422fcde76
owoicho <michaelogaje033@gmail.com>
2024-11-11 15:10:07 +0100

    added a path to wsgi.py

--- accommodation/wsgi.py
+++ accommodation/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "accommodation.settings")
 
