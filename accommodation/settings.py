027f9ec3f4d1cc3d7e3feb9099a35cf93945876d
owoicho <michaelogaje033@gmail.com>
2024-12-03 13:38:30 +0100

    changed media root to store persistently on render

--- accommodation/settings.py
+++ accommodation/settings.py
@@ -200,7 +200,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/data/media/'  # This should match the path you've selected on Render
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
