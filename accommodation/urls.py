f5c8bc2d2f6a7b53b099f28ba6722630928426b4
owoicho <michaelogaje033@gmail.com>
2024-12-06 11:29:49 +0100

    urls.py serving

--- accommodation/urls.py
+++ accommodation/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('',include('studentdashboard.url', namespace='studentdashboard'))
 
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if not settings.DEBUG:  # Only in production
+]
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
