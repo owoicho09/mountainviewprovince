0331ef8583ec1850e00920054eebaefd828a8155
owoicho <michaelogaje033@gmail.com>
2024-12-06 08:15:50 +0100

    Fix media file serving

--- accommodation/urls.py
+++ accommodation/urls.py
@@ -30,3 +30,5 @@ urlpatterns = [
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+if not settings.DEBUG:  # Only in production
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
