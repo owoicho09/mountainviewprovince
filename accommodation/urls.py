ad99ab5bc4fe53555314e7c8e9209c14ee9062b4
owoicho <michaelogaje033@gmail.com>
2024-12-06 12:33:21 +0100

    trying again

--- accommodation/urls.py
+++ accommodation/urls.py
@@ -29,6 +29,9 @@ urlpatterns = [
 
 
 ]
-if not settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG is False:  # Only add in production
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
